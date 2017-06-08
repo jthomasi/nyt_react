b3ae0104bb83487d6604ec1b355ad67aa597a9bc
Jesse Thomas <jayayetea@gmail.com>
2017-06-08 14:47:56 -0500

    initial file creation and npm install, building server.js

---
+++ models/Saved.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var SavedSchema = new Schema({
+  title: {
+    type: String
+  },
+  notes: {
+  	type: String
+  },
+  date: {
+    type: Date
+  }
+});
+
+var Saved = mongoose.model("Saved", SavedSchema);
+module.exports = Saved;
---
+++ server.js
@@ -0,0 +1,41 @@
+// require npm packages
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var mongojs = require("mongojs");
+
+// require Saved schema for mongodb
+var Saved = require("./models/Saved");
+
+// create express and create port to listen on
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// using morgan for logging purposes (boiler code)
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// set dir to static
+app.use(express.static("./public"));
+
+// creating db and connection to local mongodb using mongoose 
+mongoose.connect("mongodb://localhost/nyt_react");
+var db = mongoose.connection;
+
+db.on("error", function(error){
+	console.log("Mongoose Error " + error);
+});
+db.once("open", function(){
+	console.log("Mongoose connection successful.");
+});
+
+
+
+// listening port for localhost
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+});
